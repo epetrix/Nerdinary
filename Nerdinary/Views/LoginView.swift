375eec00c5e7b6bcf1ccadabc1e0e77d554ca831
Josh Jaslow <jbossjaslow@gmail.com>
2020-03-10 20:14:20 -0400

    Embellished Login view

--- Nerdinary/Views/LoginView.swift
+++ Nerdinary/Views/LoginView.swift
@@ -7,20 +7,120 @@
 //
 
 import SwiftUI
+import LocalAuthentication
 
 struct LoginView: View {
 	
 	@State private var username = ""
 	@State private var password = ""
+	@State private var unlocked = false
 	
     var body: some View {
 		VStack {
 			
-			TextField("Username", text: $username)
+			Text("Nerdinary")
+				.font(.system(size: 48))
+				.bold()
 			
-			TextField("Password", text: $username)
+			Spacer()
+			
+			VStack(spacing: 10) {
+				TextField("Username", text: $username)
+				.textFieldStyle(RoundedBorderTextFieldStyle())
+				
+				TextField("Password", text: $username)
+				.textFieldStyle(RoundedBorderTextFieldStyle())
+				
+				Button(action: {
+					UIApplication.shared.endEditing()
+					self.login()
+				}) {
+					HStack {
+						Spacer()
+
+						Text("Login")
+						.foregroundColor(.white)
+						.font(.system(size: 24))
+							.padding(.top, 5)
+							.padding(.bottom, 5)
+
+						Spacer()
+					}
+					.background(Color.blue)
+					.cornerRadius(4)
+					
+				}
+				
+				Button(action: {
+					self.login()
+				}) {
+					HStack {
+						Spacer()
+
+						Text("Use FaceID")
+						.foregroundColor(.white)
+						.font(.system(size: 24))
+							.padding(.top, 5)
+							.padding(.bottom, 5)
+
+						Spacer()
+					}
+					.background(Color.blue)
+					.cornerRadius(4)
+					
+				}
+			}
+			.padding(.leading, 30) //should refactor this for geometry reader at some point to make more modular
+			.padding(.trailing, 30)
+			
+			Spacer()
+			
+			VStack {
+				Button(action: {
+					//
+				}) {
+					Text("Don't have an account? Register here")
+					.underline()
+				}
+				
+				Button(action: {
+					//
+				}) {
+					Text("Forgot password? Click here")
+					.underline()
+				}
+			}
 		}
     }
+	
+	func login() {
+		unlocked = true
+	}
+	
+	func authenticate() {
+		let context = LAContext()
+		var error: NSError?
+
+		// check whether biometric authentication is possible
+		if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+			// it's possible, so go ahead and use it
+			let reason = "We need to unlock your data."
+
+			context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+				// authentication has now completed
+				DispatchQueue.main.async {
+					if success {
+						// authenticated successfully
+						self.unlocked = true
+					} else {
+						// there was a problem
+					}
+				}
+			}
+		} else {
+			// no biometrics
+		}
+	}
 }
 
 struct LoginView_Previews: PreviewProvider {
