88b07b7d251ae149121d8efda825a127f5fb91e7
Josh Jaslow <jbossjaslow@gmail.com>
2020-03-12 16:02:25 -0400

    Added Register View

---
+++ Nerdinary/Views/RegisterView.swift
@@ -0,0 +1,21 @@
+//
+//  RegisterView.swift
+//  Nerdinary
+//
+//  Created by Josh Jaslow on 3/12/20.
+//  Copyright © 2020 Josh Jaslow. All rights reserved.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RegisterView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterView()
+    }
+}
