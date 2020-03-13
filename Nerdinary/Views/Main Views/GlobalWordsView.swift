d767379542b600787573cac92656d163121eacf9
Josh Jaslow <jbossjaslow@gmail.com>
2020-03-13 11:18:41 -0400

    Added global words view

---
+++ Nerdinary/Views/Main Views/GlobalWordsView.swift
@@ -0,0 +1,22 @@
+//
+//  GlobalWordsView.swift
+//  Nerdinary
+//
+//  Created by Josh Jaslow on 3/13/20.
+//  Copyright © 2020 Josh Jaslow. All rights reserved.
+//
+
+import SwiftUI
+
+struct GlobalWordsView: View {
+	
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GlobalWordsView_Previews: PreviewProvider {
+    static var previews: some View {
+        GlobalWordsView()
+    }
+}
