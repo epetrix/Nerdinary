886af4c30a618a576710b16d5ff4ddba0ab6b611
Josh Jaslow <jbossjaslow@gmail.com>
2020-04-15 12:52:29 -0400

    Added in GET global words

--- Nerdinary/Views/Main Views/GlobalWordsView.swift
+++ Nerdinary/Views/Main Views/GlobalWordsView.swift
@@ -28,7 +28,55 @@ struct GlobalWordsView: View {
     }
 	
 	func loadGlobalWords() {
+		//TODO: - Remove transport key from info.plist, not safe for app store
+		print("Displaying global words")
 		
+		guard let url = URL(string: "http://127.0.0.1:5000/non_user_words/1002") else {
+			print("Invalid URL")
+			return
+		}
+				
+		let request = URLRequest(url: url)
+		
+		URLSession.shared.dataTask(with: request) { data, response, error in
+			// step 4
+			
+			if let data = data {
+				
+				//print("JSON String:\n \(String(data: data, encoding: .utf8) ?? "error")")
+				
+				if let decodedResponse = try? JSONDecoder().decode([DBEntryIn].self, from: data) {
+					
+					// we have good data – go back to the main thread
+					DispatchQueue.main.async {
+						if decodedResponse.isEmpty {
+							print("Empty result")
+						}
+//						 update our UI
+						self.entries.removeAll()
+						
+						for entry in decodedResponse {
+							self.entries.append(Entry(e: entry))
+						}
+						
+					}
+
+					// everything is good, so we can exit
+					return
+				} else {
+					print("problem with decoded response")
+				}
+				
+			} else {
+				print("problem with data")
+			}
+
+			// if we're still here it means there was a problem
+			print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+			//self.wordDoesntExistAlert = true
+			print("Failed")
+			
+		}.resume()
 	}
 }
 
