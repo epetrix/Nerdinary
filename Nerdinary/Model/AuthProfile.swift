33757682be0985ac978ea149ba24ab011f9550c0
Josh Jaslow <jbossjaslow@gmail.com>
2020-04-18 18:03:38 -0400

    Added necessary data models

---
+++ Nerdinary/Model/AuthProfile.swift
@@ -0,0 +1,14 @@
+//
+//  AuthProfile.swift
+//  Nerdinary
+//
+//  Created by Josh Jaslow on 4/18/20.
+//  Copyright © 2020 Josh Jaslow. All rights reserved.
+//
+
+import SwiftUI
+
+struct AuthProfile: Codable {
+	var EA: String
+	var PW: String
+}
---
+++ Nerdinary/Model/DBUserIn.swift
@@ -0,0 +1,13 @@
+//
+//  DBUserIn.swift
+//  Nerdinary
+//
+//  Created by Josh Jaslow on 4/18/20.
+//  Copyright © 2020 Josh Jaslow. All rights reserved.
+//
+
+import SwiftUI
+
+struct DBUserIn: Codable {
+	var user_id: Int
+}
