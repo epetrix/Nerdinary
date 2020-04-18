6b968a797c894b2d452e7a2ecb738858b4b18797
Josh Jaslow <jbossjaslow@gmail.com>
2020-04-18 15:25:10 -0400

    Correctly implemented POST

--- Nerdinary/Views/Login/RegisterView.swift
+++ Nerdinary/Views/Login/RegisterView.swift
@@ -70,15 +70,61 @@ struct RegisterView: View {
 		.padding([.leading, .trailing], 30)
 		.background(LinearGradient(gradient: Gradient(colors: [Color("Gradient Purple"), Color("Gradient Blue")]), startPoint: .top, endPoint: .bottom)
 		.edgesIgnoringSafeArea(.all))
+		.ableToEndEditing()
     }
 	
 	func register() {
-		showingIndicator = true
-		disableRegisterButton = true
+		let group = DispatchGroup()
+		group.enter()
 		
-		DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-			self.showingIndicator = false
-			self.presented = false
+		do {
+			showingIndicator = true
+			disableRegisterButton = true
+			let uid = Int.random(in: 1..<10000)
+			
+			print("Adding user to DB...")
+			
+			guard let url = URL(string: "http://127.0.0.1:5000/user") else {
+				print("Invalid URL")
+				group.leave()
+				return
+			}
+			
+			var request = URLRequest(url: url)
+			request.httpMethod = "POST"
+			request.setValue("application/json", forHTTPHeaderField: "Content-Type") //THIS IS SUPER NECESSARY
+			
+			let profile = DBProfile(UID: uid, FN: self.firstName, LN: self.lastName, EA: self.email, PW: self.password)
+			
+			request.httpBody = try JSONEncoder().encode(profile)
+			
+			//print(String(data: request.httpBody!, encoding: .utf8)!)
+			
+			URLSession.shared.dataTask(with: request) { (data, response, error) in
+				
+				if let error = error {
+					print("Error occurred: \(error)")
+					group.leave()
+					return
+				}
+				
+				if let data = data, let dataString = String(data: data, encoding: .utf8) {
+					DispatchQueue.main.async {
+						//print("Response data string:\n \(dataString)")
+						UserDefaults.standard.set(uid, forKey: "userID")
+						self.showingIndicator = false
+						self.disableRegisterButton = false
+						if dataString == "{}" {
+							self.presented = false
+						}
+						group.leave()
+					}
+				}
+			}.resume()
+		}
+		catch {
+			group.leave()
+			fatalError("Couldn't encode JSON")
 		}
 	}
 }
