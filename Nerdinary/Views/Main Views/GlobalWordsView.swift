7f3c1b7408e2fa5457edcfd68bccf3f56da09c66
Josh Jaslow <jbossjaslow@gmail.com>
2020-05-01 18:33:50 -0400

    Moved to global words vm

--- Nerdinary/Views/Main Views/GlobalWordsView.swift
+++ Nerdinary/Views/Main Views/GlobalWordsView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct GlobalWordsView: View {
 	
-	@State private var entries: [Entry] = [Entry]()
+	@ObservedObject var globalWordsVM = GlobalWordsVM()
 	
     var body: some View {
 		GeometryReader { geometry in
@@ -26,77 +26,11 @@ struct GlobalWordsView: View {
 				}
 				.padding([.leading, .top])
 				
-				WordsListView(entries: self.$entries, loadMethod: self.loadGlobalWords, isGlobal: true)
+				WordsListView(entries: self.$globalWordsVM.entries, loadMethod: self.globalWordsVM.loadGlobalWords, isGlobal: true)
 			}
 			.background(LinearGradient(gradient: Gradient(colors: [Color("Color Scheme Orange"), Color("Color Scheme Red")]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all))
 		}
     }
-	
-	func loadGlobalWords() {
-		let group = DispatchGroup()
-		group.enter()
-		
-		//TODO: - Remove transport key from info.plist, not safe for app store
-		print("Displaying global words")
-		
-		let uid = UserDefaults.standard.integer(forKey: "userID")
-		if uid == 0 {
-			print("Invalid User ID")
-			group.leave()
-			return
-		}
-		
-		guard let url = URL(string: "http://127.0.0.1:5000/non_user_words/\(uid)") else {
-			print("Invalid URL")
-			group.leave()
-			return
-		}
-				
-		let request = URLRequest(url: url)
-		
-		URLSession.shared.dataTask(with: request) { data, response, error in
-			// step 4
-			
-			if let data = data {
-				
-				//print("JSON String:\n \(String(data: data, encoding: .utf8) ?? "error")")
-				
-				if let decodedResponse = try? JSONDecoder().decode([DBEntry].self, from: data) {
-					
-					// we have good data – go back to the main thread
-					DispatchQueue.main.async {
-						if decodedResponse.isEmpty {
-							print("Empty result")
-						}
-//						 update our UI
-						self.entries.removeAll()
-						
-						for entry in decodedResponse {
-							self.entries.append(Entry(e: entry))
-						}
-						
-						group.leave()
-					}
-
-					// everything is good, so we can exit
-					return
-				} else {
-					print("problem with decoded response")
-					group.leave()
-				}
-				
-			} else {
-				print("problem with data")
-				group.leave()
-			}
-
-			// if we're still here it means there was a problem
-			print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
-			//self.wordDoesntExistAlert = true
-			print("Failed")
-			
-		}.resume()
-	}
 }
 
 struct GlobalWordsView_Previews: PreviewProvider {
