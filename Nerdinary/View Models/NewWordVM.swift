e867622a2b107f12c85cadceb7b9dfdd0f00f3c2
Josh Jaslow <jbossjaslow@gmail.com>
2020-05-01 18:35:08 -0400

    Added vm for new word view

---
+++ Nerdinary/View Models/NewWordVM.swift
@@ -0,0 +1,13 @@
+//
+//  NewWordVM.swift
+//  Nerdinary
+//
+//  Created by Josh Jaslow on 5/1/20.
+//  Copyright © 2020 Josh Jaslow. All rights reserved.
+//
+
+import SwiftUI
+
+class NewWordVM: ObservableObject {
+	
+}
